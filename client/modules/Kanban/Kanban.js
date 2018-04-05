5aa5a40d6f9f113497147627da7460d0022e20ad
Dawid Brzozowski <d.brzozowski@wp.pl>
2018-04-05 20:09:09 +0200

    Add Rest API

---
+++ client/modules/Kanban/Kanban.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './Kanban.css';
+
+class Kanban extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+Kanban.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Kanban);
---
+++ client/modules/Lane/Lane.js
@@ -0,0 +1,29 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './Lane.css';
+
+class Lane extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+Lane.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Lane);
